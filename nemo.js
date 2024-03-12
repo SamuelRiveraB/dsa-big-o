6c752d3e2b8ab2f8fa509da2e252008cf00307d0
Samuel <samuelrivba@gmail.com>
2024-03-12 15:58:19 -0500

    Add nemo file

---
+++ nemo.js
@@ -0,0 +1,23 @@
+const nemo = ["nemo"];
+const everyone = [
+  "dory",
+  "bruce",
+  "marlin",
+  "nemo",
+  "gill",
+  "bloat",
+  "nigel",
+  "squirt",
+  "darla",
+  "hank",
+];
+
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "nemo") {
+      console.log("Found NEMO!");
+    }
+  }
+}
+
+findNemo(nemo);
