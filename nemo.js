59acb0598d7cb43cdb861d4381d84fd136c67392
Samuel <samuelrivba@gmail.com>
2024-03-12 16:01:00 -0500

    Add large array and performance testing

--- nemo.js
+++ nemo.js
@@ -11,13 +11,17 @@ const everyone = [
   "darla",
   "hank",
 ];
+const large = new Array(100000).fill("nemo");
 
 function findNemo(array) {
+  let t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === "nemo") {
       console.log("Found NEMO!");
     }
   }
+  let t1 = performance.now();
+  console.log(`Call to find Nemo took ${t1 - t0} milliseconds`);
 }
 
-findNemo(nemo);
+findNemo(large);
